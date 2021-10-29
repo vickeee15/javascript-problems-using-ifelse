7a82aa0001c8a0d3e3065a7f9251ca3c62265018
vickeee15 <89180138+vickeee15@users.noreply.github.com>
2021-10-29 20:24:57 +0530

    Add files via upload
    finding max and min number among 5 random 3 digit number using if else

---
+++ min-and-max-number.js
@@ -0,0 +1,37 @@
+let n1 = (Math.floor(Math.random() * 900 +100));
+let n2 = (Math.floor(Math.random() * 900 +100));
+let n3 = (Math.floor(Math.random() * 900 +100));
+let n4 = (Math.floor(Math.random() * 900 +100));
+let n5 = (Math.floor(Math.random() * 900 +100));
+
+if (n1 >= n2 && n1 >= n3 && n1 >= n4 && n1 >= n5) {
+    console.log("n1 is largest and it is" +n1);
+} 
+if (n2 >= n1 && n2 >= n3 && n2 >= n4 && n2 >= n5) {
+    console.log("n2 is largest and it is" +n2);
+} 
+if (n3 >= n1 && n3 >= n2 && n3 >= n4 && n3 >= n5) {
+    console.log("n3 is largest and it is" +n3);
+} 
+if (n4 >= n1 && n4 >= n2 && n4 >= n3 && n4 >= n5) {
+    console.log("n4 is largest and it is" +n4);
+} 
+if (n5 >= n1 && n5 >= n2 && n5 >= n3 && n5 >= n4) {
+    console.log("n5 is largest and it is" +n5);
+} 
+
+if (n1 <= n2 && n1 <= n3 && n1 <= n4 && n1 <= n5) {
+    console.log("n1 is minimum and it is" +n1);
+}
+if (n2 <= n1 && n2 <= n3 && n2 <= n4 && n2 <= n5) {
+    console.log("n2 is minimum and it is" +n2);
+} 
+if (n3 <= n1 && n3 <= n2 && n3 <= n4 && n3 <= n5) {
+    console.log("n3 is minimum and it is" +n3);
+} 
+if (n4 <= n1 && n4 <= n2 && n4 <= n3 && n4 <= n5) {
+    console.log("n4 is minimum and it is" +n4);
+} 
+if (n5 <= n1 && n5 <= n2 && n5 <= n3 && n5 <= n4) {
+    console.log("n5 is minimum and it is" +n4);
+} 
